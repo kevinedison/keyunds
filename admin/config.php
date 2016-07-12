9eca45a6540ee4497bf8b35032a4882b8997664a
lyx5710317 <315851414@qq.com>
2016-07-12 14:35:58 +0800

    text
    l

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/admin/');
+define('HTTP_CATALOG', 'http://127.0.0.1/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/admin/');
+define('HTTPS_CATALOG', 'http://127.0.0.1/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/admin/');
+define('DIR_SYSTEM', 'C:/wamp/www/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp/www/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/image/');
+define('DIR_CACHE', 'C:/wamp/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp/www/system/storage/upload/');
+define('DIR_CATALOG', 'C:/wamp/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'kira');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
